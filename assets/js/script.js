1a8d4bef460fda380031f8a63098d1d0a7937976
mvergara94 <mariovergaralorena@gmail.com>
2023-05-12 15:35:53 -0300

    Upando arquivos iniciais Poketrivia

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+const board = document.querySelector('.board__img');
+const startTriviaBtn = document.querySelector('#start-trivia-btn');
+const triviaOptions = document.querySelector('.board__options');
+
+startTriviaBtn.addEventListener('click', async () => {
+  const pokemonNumbers = generateRandomNumbers();
+  const pokemonPromises = pokemonNumbers.map(number =>
+    fetch(`https://pokeapi.co/api/v2/pokemon/${number}`).then(response =>
+      response.json()
+    )
+  );
+  const pokemons = await Promise.all(pokemonPromises);
+  const correctPokemon = pokemons[0];
+  const correctPokemonName = pokemons[0].name;
+  showPokemon(correctPokemon.sprites.front_default);
+  showTriviaOptions(shuffleArray(pokemons), correctPokemonName);
+});
+
+function generateRandomNumbers() {
+  const randomNumbers = [];
+  let i;
+  for (i = 0; i < 4; i++) {
+    const randomNumber = Math.floor(Math.random() * 150 + 1);
+    randomNumbers.push(randomNumber);
+  }
+
+  return randomNumbers;
+}
+
+function showPokemon(pokeImg) {
+  board.innerHTML = `<img src="${pokeImg}">`;
+}
+
+function showTriviaOptions(pokemons, correctPokemonName) {
+  triviaOptions.innerHTML = '';
+  pokemons.forEach(pokemon => {
+    const option = document.createElement('button');
+    option.textContent = pokemon.name;
+    option.addEventListener('click', () => {
+      if (option.textContent === correctPokemonName) {
+        console.log('Correct!');
+      } else {
+        console.log('Wrong!');
+      }
+    });
+    triviaOptions.appendChild(option);
+  });
+}
+
+function shuffleArray(array) {
+  const newArray = [...array];
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+}

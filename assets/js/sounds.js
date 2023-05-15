2a26a8541a735aca4f4d179d20d7a43083348653
mvergara94 <mariovergaralorena@gmail.com>
2023-05-15 15:42:08 -0300

    Colocando arquivos de audio

---
+++ assets/js/sounds.js
@@ -0,0 +1,4 @@
+const playPauseBtn = document.getElementById('playPauseBtn');
+const previousBtn = document.getElementById('previousBtn');
+const nextBtn = document.getElementById('nextBtn');
+const tracks = document.querySelectorAll('.track');

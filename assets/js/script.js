b965b9bb6a21f6fef2b73f667c5f303a21d21a29
mvergara94 <mariovergaralorena@gmail.com>
2023-05-17 17:08:26 -0300

    Testando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
 import { startGame } from './logic.js';
 
-window.addEventListener('resize', adjustBodyHeight);
-
-function adjustBodyHeight() {
-  const bodyElement = document.body;
-  const navigationBarHeight = window.innerHeight - window.visualViewport.height;
-
-  bodyElement.style.height = `calc(100vh - ${navigationBarHeight}px)`;
-}
-
-adjustBodyHeight();
-
 startGame();

9d689074676911ef82673ee52ac05d092ef5e4f5
mvergara94 <mariovergaralorena@gmail.com>
2023-05-17 15:42:51 -0300

    Utilizando JS para arrumar altura

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 import { startGame } from './logic.js';
 
+window.addEventListener('resize', adjustBodyHeight);
+
+function adjustBodyHeight() {
+  const bodyElement = document.body;
+  const navigationBarHeight =
+    window.innerHeight - document.documentElement.clientHeight;
+
+  bodyElement.style.height = `calc(100vh - ${navigationBarHeight}px)`;
+}
+
+adjustBodyHeight();
+
 startGame();

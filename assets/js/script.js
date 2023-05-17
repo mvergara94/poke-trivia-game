e12ead47e50727ea6b2a0c0dfb57cbf9b9ef0159
mvergara94 <mariovergaralorena@gmail.com>
2023-05-17 17:04:48 -0300

    Só consigo testar no meu celular no gitPages

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,7 @@ window.addEventListener('resize', adjustBodyHeight);
 
 function adjustBodyHeight() {
   const bodyElement = document.body;
-  const navigationBarHeight =
-    window.innerHeight - document.documentElement.clientHeight;
+  const navigationBarHeight = window.innerHeight - window.visualViewport.height;
 
   bodyElement.style.height = `calc(100vh - ${navigationBarHeight}px)`;
 }
